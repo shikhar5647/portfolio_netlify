dfe9f2ecaa3a7f5953ec4649e6eef5e1f797e443
Shikhar Dave <120463805+shikhar5647@users.noreply.github.com>
2024-06-20 18:33:20 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -11,12 +11,12 @@ const About = () => {
                 </p>
             </div>
             <p className='text-xl mt-15'>
-            Welcome readers ! I'm Aditya Sahani, a passionate and driven computer science engineering student at the Indian Institute of Technology, Jodhpur.
-           With a strong foundation in computer science and a keen interest in cutting-edge technologies, I strive to make a positive impact in the field of artificial intelligence and machine learning. 
+            Welcome folks ! I'm Shikhar Dave, a passionate Machine Learning Enthusiast and a student at the Indian Institute of Technology, Jodhpur.
+           With a strong foundation in DL and a keen interest in cutting-edge technologies, I strive to make a positive impact in the field of artificial intelligence and machine learning. 
            Throughout my academic journey, I have consistently excelled in various subjects, including data structures and algorithms, machine learning, and deep learning.
             These skills have enabled me to tackle complex challenges and develop innovative solutions. 
             I have been fortunate to work on diverse projects that span different domains, showcasing my ability to adapt and apply my knowledge effectively.
-       In addition to my academic pursuits, I have been actively involved in various extracurricular activities and leadership roles. As the Assitant Head of the Entrepreneurship Cell at IITJ, I have fostered a culture of innovation and supported budding entrepreneurs in bringing their ideas to life.
+       In addition to my academic pursuits, I have been actively involved in various extracurricular activities and leadership roles. As the Internship Coordinator at the Career Development Cell at IITJ, I am overseeing student internships, placements, and maintaining the official website. Dedicated to facilitating student career advancement.
        
 
 If you share similar passions or have exciting opportunities to explore, feel free to reach out. Let's connect and embark on a journey to drive positive change through technology!
